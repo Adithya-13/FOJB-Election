0ecd4868ba4a0702acbfd801a3c07f4d1207bd3f
adithyafirmansyahputra <aditputrafirmansyah@gmail.com>
2021-09-16 17:19:04 +0700

    create header detail page

--- lib/presentation/pages/detail_page.dart
+++ lib/presentation/pages/detail_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fojb_election/presentation/utils/utils.dart';
+import 'package:fojb_election/presentation/widgets/custom_button.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -23,11 +24,70 @@ class DetailPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
+          padding: EdgeInsets.all(Helper.normalPadding),
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children: [],
+            children: [
+              _headerDetail(context),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _headerDetail(BuildContext context){
+    return Container(
+      child: Row(
+        children: [
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: Helper.getShadow(),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    Resources.imgDummy,
+                    fit: BoxFit.cover
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: Helper.normalPadding),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Farah Fauziah Danopa',
+                  style: AppTheme.headline3,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: Helper.smallPadding),
+                Text(
+                  'Kab. Majalengka',
+                  style: AppTheme.text2,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: Helper.smallPadding),
+                Text(
+                  'SMA Negeri 1 Majalengka',
+                  style: AppTheme.text2,
+                ),
+                SizedBox(height: Helper.normalPadding),
+                CustomButton(onTap: () {}, text: 'Vote Farah'),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
