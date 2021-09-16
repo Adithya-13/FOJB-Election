bb7bf48e36e9826655a766c35e1e904c5d079771
adithyafirmansyahputra <aditputrafirmansyah@gmail.com>
2021-09-16 18:16:42 +0700

    create after vote page

--- lib/presentation/pages/after_vote_page.dart
+++ lib/presentation/pages/after_vote_page.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:fojb_election/presentation/routes/routes.dart';
+import 'package:fojb_election/presentation/utils/utils.dart';
+import 'package:fojb_election/presentation/widgets/widgets.dart';
 
-class AfterVotePage extends StatelessWidget {
+class AfterVotePage extends StatefulWidget {
   const AfterVotePage({Key? key}) : super(key: key);
 
   @override
+  _AfterVotePageState createState() => _AfterVotePageState();
+}
+
+class _AfterVotePageState extends State<AfterVotePage> {
+  static const Duration _navigateDelay = Duration(seconds: 3);
+
+  @override
+  void initState() {
+    _navigateOtherScreen();
+    super.initState();
+  }
+
+  void _navigateOtherScreen() {
+    Future.delayed(_navigateDelay).then(
+      (_) => Navigator.pushNamedAndRemoveUntil(
+          context, PagePath.home, (route) => false),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  Resources.fojbLogo,
+                  width: MediaQuery.of(context).size.width * 0.3,
+                ),
+                Text(
+                  'FOJB Election',
+                  style: AppTheme.text1.white,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Text('Terima kasih telah Voting!', style: AppTheme.headline2.white),
+          ],
+        ),
+      ),
+    );
   }
 }
